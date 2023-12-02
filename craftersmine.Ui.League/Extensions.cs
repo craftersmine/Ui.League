5aab7539588437f00b69787a48686432d90aa6eb
craftersmine <rodion.morozov.01@gmail.com>
2023-12-03 03:57:03 +0700

    Added internal extensions

---
+++ craftersmine.Ui.League/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using craftersmine.Ui.League.Controls;
+
+namespace craftersmine.Ui.League
+{
+    internal static class WindowExtensions
+    {
+        public static void ForWindowFromTemplate(this object templateFrameworkElement, Action<Window> action)
+        {
+            Window window = ((FrameworkElement)templateFrameworkElement).TemplatedParent as Window;
+            if (window != null) action(window);
+        }
+
+        public static IntPtr GetWindowHandle(this LeagueWindow window)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(window);
+            return helper.Handle;
+        }
+    }
+}
